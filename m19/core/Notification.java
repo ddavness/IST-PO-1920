f578a718d4a90c99de6a8325f47153a398b42a1a
Bernardo Mota <bernardo.mota@tecnico.ulisboa.pt>
2019-10-31 11:51:51 +0000

    Implements Notification class.

---
+++ m19/core/Notification.java
@@ -0,0 +1,19 @@
+package m19.core;
+
+/**
+ * Notification - Is just a simple String.
+ * Implemented according to teachers' UML.
+ * @version 0.0
+ */
+public class Notification {
+
+    private final String _message;
+
+    public Notification(String message) {
+        _message = message;
+    }
+
+    public String getMessage() {
+        return _message;
+    }
+}
