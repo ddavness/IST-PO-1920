86b3859ddd9f232ab57b16370b23d37eb3a6c620
Bernardo Mota <bernardo.mota@tecnico.ulisboa.pt>
2019-11-16 19:23:28 +0000

    Implements Rule abstract class.

--- m19/core/Rule.java
+++ m19/core/Rule.java
@@ -4,7 +4,16 @@ package m19.core;
  * Rule
  */
 public abstract class Rule {
-
-    //FIXME Implement
+    private final int _id;
+    public Rule(int id) {
+        _id = id;
+    }
+    /**
+     * 
+     * @param user
+     * @param work
+     * @return true iff the work can be requested by the user
+     */
+    public abstract boolean isValid(User user, Work work);
 
 }
