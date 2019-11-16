38f72a51c786959ead082bbef0b2d9b04c73c188
Bernardo Mota <bernardo.mota@tecnico.ulisboa.pt>
2019-11-16 23:07:48 +0000

    Implements CheckWorkIsLowValue

---
+++ m19/core/rules/CheckWorkIsLowValue.java
@@ -0,0 +1,23 @@
+package m19.core.rules;
+import m19.core.Rule;
+import m19.core.Library;
+import m19.core.Request;
+/**
+ * CheckWorkIsLowValue
+ */
+public class CheckWorkIsLowValue extends Rule {
+
+    public CheckWorkIsLowValue(int id, Library library) {
+        super(id, library);
+    }
+
+    /**
+     * @return a user is suspended iff it is not active
+     */
+
+    public boolean isValid(Request request) {
+        int MAX_PRICE = 25; // in euros
+        return request.getWork().getPrice() <= MAX_PRICE;
+    }
+    
+}
