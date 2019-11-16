59bf4db78821c3337c405f30cb8a37ceb2d7256a
David Duque <david.f.s.duque@tecnico.ulisboa.pt>
2019-11-16 16:39:18 +0000

    Implement DisplayWork

--- m19/app/works/DoDisplayWork.java
+++ m19/app/works/DoDisplayWork.java
@@ -1,28 +1,41 @@
 package m19.app.works;
 
+import java.util.List;
+import m19.core.Work;
+import m19.app.exception.NoSuchWorkException;
 import m19.core.LibraryManager;
 import pt.tecnico.po.ui.DialogException;
 import pt.tecnico.po.ui.Command;
+import pt.tecnico.po.ui.Input;
 
 /**
  * 4.3.1. Display work.
  */
 public class DoDisplayWork extends Command<LibraryManager> {
 
-    // FIXME define input fields
+    private Input<Integer> _input;
 
     /**
      * @param receiver
      */
     public DoDisplayWork(LibraryManager receiver) {
         super(Label.SHOW_WORK, receiver);
-        // FIXME initialize input fields
+        _input = _form.addIntegerInput(Message.requestWorkId());
     }
 
     /** @see pt.tecnico.po.ui.Command#execute() */
     @Override
     public final void execute() throws DialogException {
-        // FIXME implement command
+        _form.parse();
+        int index = _input.value();
+        List<Work> works = _receiver.getAllWorks();
+
+        if (works.size() <= index) {
+            throw new NoSuchWorkException(index);
+        }
+
+        _display.add(works.get(index).getDescription());
+        _display.display();
     }
 
 }
